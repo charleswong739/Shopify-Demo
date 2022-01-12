a455e18e312fe965f196081488e8f98ddb9d4d95
Charles Wong <charleswonghm@gmail.com>
2022-01-11 21:33:21 -0500

    Create item repository

---
+++ src/main/java/com/chwonghm/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.chwonghm.repository;
+
+import com.chwonghm.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Interface defining item database interaction methods. Methods defined
+ * here are automatically generated implementations by Spring
+ * <p>
+ * Extending JpaRepository also provides some default repository methods.
+ *
+ * @author Charles Wong
+ */
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
